eb770dc7b18c7962f5f890738d729ce1ebdd75bc
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-03-02 06:37:09 +0300

    added.

---
+++ admin/language/tr-tr/extension/module/store.php
@@ -0,0 +1,20 @@
+<?php
+/* Turkceye Ceviren eka7a - http://e-piksel.com */
+
+// Heading
+$_['heading_title']    = 'Mağazalar';
+
+// Text
+$_['text_extension']   = 'Eklentiler';
+$_['text_success']     = 'Başarılı: Mağazalar modülü güncellendi!';
+$_['text_edit']        = 'Mağaza Modülünü Düzenle';
+
+// Entry
+$_['entry_admin']      = 'Sadece Yöneticiler';
+$_['entry_status']     = 'Durumu';
+
+//Help
+$_['help_admin']       = 'Çoklu mağaza listesi yalnızca yönetici kullanıcı oturum açtığı zaman görünür olacaktır!';
+
+// Error
+$_['error_permission'] = 'Uyarı: Mağazalar modülünü değiştirme iznine sahip değilsiniz!';
