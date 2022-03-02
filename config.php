d5cbd51e43dc3113bc4ef0ad6da8d14f26e5b3b3
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-03-02 05:26:03 +0300

    added.

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/proje/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/proje/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/sanalhost/htdocs/proje/catalog/');
+define('DIR_SYSTEM', 'C:/sanalhost/htdocs/proje/system/');
+define('DIR_IMAGE', 'C:/sanalhost/htdocs/proje/image/');
+define('DIR_STORAGE', 'C:/sanalhost/datas/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'proje');
+define('DB_PASSWORD', 'Bt.0102034');
+define('DB_DATABASE', 'proje');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
