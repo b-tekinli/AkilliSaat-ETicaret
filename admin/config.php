17056b69f65d8645e066d2078f97aeff7429f87c
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-03-02 05:48:13 +0300

    added.

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/proje/admin/');
+define('HTTP_CATALOG', 'http://localhost/proje/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/proje/admin/');
+define('HTTPS_CATALOG', 'http://localhost/proje/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/sanalhost/htdocs/proje/admin/');
+define('DIR_SYSTEM', 'C:/sanalhost/htdocs/proje/system/');
+define('DIR_IMAGE', 'C:/sanalhost/htdocs/proje/image/');
+define('DIR_STORAGE', 'C:/sanalhost/datas/');
+define('DIR_CATALOG', 'C:/sanalhost/htdocs/proje/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'proje');
+define('DB_PASSWORD', 'Bt.0102034');
+define('DB_DATABASE', 'proje');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
