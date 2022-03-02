1aaf7f98653e582a09a46119acf45af0862090ae
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-03-02 06:33:18 +0300

    added.

---
+++ admin/language/tr-tr/common/dashboard.php
@@ -0,0 +1,8 @@
+<?php
+/* Turkceye Ceviren eka7a - http://e-piksel.com */
+
+// Heading
+$_['heading_title']       = 'Yönetim Paneli';
+
+// Error
+$_['error_install']       = 'UYARI: INSTALL DİZİNİ HALA MEVCUT VE GÜVENLİK NEDENİ İLE SİLMENİZ TAVSİYE EDİLİR!';
