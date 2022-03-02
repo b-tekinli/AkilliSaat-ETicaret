633223d3853b3f06e9b98ec088f11a6ab64838e1
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-03-02 06:34:30 +0300

    added.

---
+++ admin/language/tr-tr/mail/transaction.php
@@ -0,0 +1,8 @@
+<?php
+/* Turkceye Ceviren eka7a - http://e-piksel.com */
+
+// Text
+$_['text_subject']  = '%s - Hesap Bakiyeniz';
+$_['text_received'] = '%s kredi aldınız!';
+$_['text_total']    = 'Kazandığınız krediyle birlikte toplam krediniz %s.';
+$_['text_credit']   = 'Kazandığınız krediler sonraki alışverişinizde hesabınızdan otomatik olarak düşülür.';
